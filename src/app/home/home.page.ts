f2de17161a26d74d655ab0dd4880f1f66d9a1f31
Montserrat Aguilar <maguilar14@ucol.mx>
2019-05-28 16:33:47 -0500

    ion-menu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { DataService } from '../services/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,83 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  Componentes:Componente[] = [
-    {
-      icon: 'american-football',
-      name: 'Action Sheet',
-      redirectTo: '/action-sheet'
-    },
-    {
-      icon: 'appstore',
-      name: 'Alert',
-      redirectTo: '/alert'
-    },
-    {
-      icon: 'beaker',
-      name: 'Avatar',
-      redirectTo: '/avatar'
-    },
-    {
-      icon: 'radio-button-on',
-      name: 'Botones y router',
-      redirectTo: '/botones'
-    },
-    {
-      icon: 'card',
-      name: 'Cards',
-      redirectTo: '/card'
-    },
-    {
-      icon: 'checkmark-circle-outline',
-      name: 'Checkbox',
-      redirectTo: '/check'
-    },
-    {
-      icon: 'calendar',
-      name: 'DateTime',
-      redirectTo: '/date-time'
-    },
-    {
-      icon: 'car',
-      name: 'Fabs',
-      redirectTo: '/fab'
-    },
-    {
-      icon: 'grid',
-      name: 'Grid-Rows',
-      redirectTo: '/grid'
-    },
-    {
-      icon: 'Infinite',
-      name: 'Infinite Scroll',
-      redirectTo: '/infinite-scroll'
-    },
-    {
-      icon: 'hammer',
-      name: 'Input - Forms',
-      redirectTo: '/input'
-    },
-    {
-      icon: 'list',
-      name: 'Listas - Sliding',
-      redirectTo: '/list'
-    },
-    {
-      icon: 'reorder',
-      name: 'Listas - Reorder',
-      redirectTo: '/list-order'
-    },
-    {
-      icon: 'refresh-circle',
-      name: 'Loading',
-      redirectTo: '/loading'
-    }
-  ];
 
-  constructor() { }
+  Componentes: Observable<Componente[]>;
+
+  constructor( private menuCtrl: MenuController,
+                private dataService: DataService) { }
 
   ngOnInit() {
+    this.Componentes = this.dataService.getMenuOpts ();
+  }
 
+  toggleMenu () {
+    this.menuCtrl.toggle();
   }
 }
 
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Componente } from '../interfaces/interfaces';
 
 
 @Injectable({
@@ -10,8 +11,12 @@ export class DataService {
   constructor( private http: HttpClient) { }
 
   //método que permite obtener los usuarios
-  
   getUsers () {
     return this.http.get ('https://jsonplaceholder.typicode.com/users');
+  }
+
+  //MÉTODO QUE REGRESA UN ARREGLO DE COMPONENTE
+  getMenuOpts() {
+    return this.http.get<Componente[]>( '/assets/data/menu.json');
   }
 }
