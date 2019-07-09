331c295269c9aa19c59de91cd688ec0ce078ac22
Montserrat Aguilar <maguilar14@ucol.mx>
2019-07-09 11:34:57 -0500

    ion-searchbar - Aplicando el filtro en la lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ComponentsModule } from './components/components.module';
 import { HttpClientModule} from '@angular/common/http';
+import { PipesModule } from './pipes/pipes.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,6 +20,7 @@ import { HttpClientModule} from '@angular/common/http';
     IonicModule.forRoot(),
     AppRoutingModule,
     ComponentsModule,
+    PipesModule,
     HttpClientModule
 
   ],
--- src/app/pages/searchbar/searchbar.page.ts
+++ src/app/pages/searchbar/searchbar.page.ts
@@ -9,6 +9,7 @@ import { DataService } from '../../services/data.service';
 export class SearchbarPage implements OnInit {
 
   albumes: any[] = [];
+  textoBuscar = '';
 
   constructor( private dataService: DataService) { }
 
@@ -22,6 +23,6 @@ export class SearchbarPage implements OnInit {
   }
 
   buscar ( event ) {
-    console.log ( event );
+    this.textoBuscar = event.detail.value;
   }
 }
